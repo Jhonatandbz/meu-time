965995e94b415f70c9159942a913d7622fbf938d
Jhonatan Oliveira <jhonatanoliveira315@gmail.com>
2023-05-26 17:31:34 -0300

    adiçao player

---
+++ src/components/players.tsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from "react";
+import {makeGetRequest} from "../adapters/get"
+import { PlayersProps, PlayerResponse} from "../interfaces/players";
+
+import database from "../json/players.json"
+
+const PlayerList: React.FC<PlayersProps> = ({league, team}) => {
+
+    const [data, setData] = useState<PlayerResponse | null>(null)
+
+    useEffect(()=>{
+
+        const leagueSeason = league.split(',')
+        const endpoint = `players?team=${team}&league=${encodeURIComponent(leagueSeason[0])}&season=${encodeURIComponent(leagueSeason[1])}`
+        console.log()
+
+        const fetchData = async () =>{
+            try{
+                const response = await makeGetRequest({endpoint: endpoint});
+                setData(response);
+            }catch(error){
+                console.error(error);
+            }
+        }
+
+        if(league && team){
+            fetchData();
+        }
+
+    }, [team, league])
+
+    // if (!data) {
+    //     return <div>Carregando...</div>;
+    // }
+
+    return(
+        <>
+        <ul>
+            {database?.response.map(({player}) => (
+                <li key={player.id}>
+                   <h4>Name: {player.firstname}</h4>
+                   <p>Age: {player.age} - Country: {player.nationality}</p>
+                </li>
+            ))}
+        </ul>
+        </>
+    )
+}
+
+export default PlayerList;
---
+++ src/interfaces/players.ts
@@ -0,0 +1,115 @@
+export interface PlayerResponse {
+    get: string;
+    parameters: {
+      team: string;
+      league: string;
+      season: string;
+    };
+    errors: any[];
+    results: number;
+    paging: {
+      current: number;
+      total: number;
+    };
+    response: PlayerItem[];
+  }
+  
+export interface PlayerItem {
+    player: {
+      age: number;
+      birth: {
+        country: string;
+        date: string;
+        place: string | null;
+      };
+      firstname: string;
+      height: number | null;
+      id: number;
+      injured: boolean;
+      lastname: string;
+      name: string;
+      nationality: string;
+      photo: string;
+      weight: number | null;
+    };
+    statistics: {
+      cards: {
+        red: number | null;
+        yellow: number | null;
+        yellowred: number | null;
+      };
+      dribbles: {
+        attempts: number | null;
+        past: number | null;
+        success: number | null;
+      };
+      duels: {
+        total: number | null;
+        won: number | null;
+      };
+      fouls: {
+        committed: number | null;
+        drawn: number | null;
+      };
+      games: {
+        appearences: number | null;
+        captain: boolean;
+        lineups: number | null;
+        minutes: number | null;
+        number: number | null;
+        position: string;
+        rating: number | null;
+      };
+      goals: {
+        assists: number | null;
+        conceded: number | null;
+        saves: number | null;
+        total: number | null;
+      };
+      league: {
+        country: string;
+        flag: string;
+        id: number;
+        logo: string;
+        name: string;
+        season: number;
+      };
+      passes: {
+        accuracy: number | null;
+        key: number | null;
+        total: number | null;
+      };
+      penalty: {
+        commited: number | null;
+        missed: number | null;
+        saved: number | null;
+        scored: number | null;
+        won: number | null;
+      };
+      shots: {
+        on: number | null;
+        total: number | null;
+      };
+      substitutes: {
+        bench: number | null;
+        in: number | null;
+        out: number | null;
+      };
+      tackles: {
+        blocks: number | null;
+        interceptions: number | null;
+        total: number | null;
+      };
+    };
+    team: {
+      id: number;
+      logo: string;
+      name: string;
+    };
+}
+
+export interface PlayersProps{
+    league: string
+    team: string
+}
+
