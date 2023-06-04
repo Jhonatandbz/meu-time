65fba05fd451808949f15521513d0b272ba786c2
Jhonatan Oliveira <jhonatanoliveira315@gmail.com>
2023-06-04 18:01:53 -0300

    ajuste readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Login from "./pages/"
 import MainPage from "./pages/App"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -9,13 +8,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const basename = process.env.PUBLIC_URL || '/';
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/meutime' element={<MainPage />} />
       </Routes>
     </BrowserRouter>
 );
-
-reportWebVitals();
